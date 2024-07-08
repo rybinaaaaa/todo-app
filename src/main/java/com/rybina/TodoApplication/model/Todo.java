17583f4189dbe3b222d8a3e34e56bf8cd5e4cffc
rybinali <rybinaaaaa.a@gmail.com>
2024-07-08 18:14:11 +0200

    add model

--- src/main/java/com/rybina/TodoApplication/model/Todo.java
+++ src/main/java/com/rybina/TodoApplication/model/Todo.java
@@ -1,4 +1,25 @@
 package com.rybina.TodoApplication.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    private LocalDate targetDate;
+
+    private Boolean isDone;
+
+    @JoinColumn(referencedColumnName = "id", name = "user_id")
+    @ManyToOne
+    private User user;
 }
--- src/main/java/com/rybina/TodoApplication/model/User.java
+++ src/main/java/com/rybina/TodoApplication/model/User.java
@@ -1,4 +1,35 @@
 package com.rybina.TodoApplication.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Todo> todoList = new ArrayList<>();
+
+    public void addTodos(Todo... todos) {
+        todoList.addAll(Arrays.asList(todos));
+    }
+
+    public void removeTodo(Todo... todos) {
+        Arrays.asList(todos).forEach((todo) -> {
+            if (!todoList.contains(todo)) throw new IllegalArgumentException();
+            else todoList.remove(todo);
+        });
+    }
 }
